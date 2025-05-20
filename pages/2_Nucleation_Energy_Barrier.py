ab682f1b56af043698d049bc620cd743951bb7f2
Bruno M. Guerreiro <33891979+cryobiochem@users.noreply.github.com>
2025-05-20 22:40:55 +0100

    Update 2_Nucleation_Energy_Barrier.py

--- pages/2_Nucleation_Energy_Barrier.py
+++ pages/2_Nucleation_Energy_Barrier.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import pandas as pd
 
-st.set_page_config(
-    page_title="Nucleation Energy Barrier - Cryopreservation Research Hub",
-    page_icon="❄️",
-    layout="wide"
-)
-
 st.title("Nucleation Activation Energy Barrier Calculator")
 
 st.markdown("""
